8e9520167684909e3cb9a238d71984b0ac305bdf
shivabalasani <shiva.balasani@gmail.com>
2019-12-10 07:05:06 -0500

    Added Drag and Drop

--- assets/scripts/projects/adv-dom.js
+++ assets/scripts/projects/adv-dom.js
@@ -84,6 +84,7 @@ class DOMHelper {
       this.updateProjectListsHandler = updateProjectListsFunction;
       this.connectMoreInfoButton();
       this.connectSwitchButton(type);
+      this.connectDrag();
     }
   
     showMoreInfoHandler() {
@@ -101,6 +102,18 @@ class DOMHelper {
       );
       tooltip.attach();
       this.hasActiveTooltip = true;
+    }
+  
+    connectDrag() {
+      const item = document.getElementById(this.id);
+      item.addEventListener('dragstart', event => {
+        event.dataTransfer.setData('text/plain', this.id);
+        event.dataTransfer.effectAllowed = 'move';
+      });
+  
+      item.addEventListener('dragend', event => {
+        console.log(event);
+      });
     }
   
     connectMoreInfoButton() {
@@ -140,6 +153,43 @@ class DOMHelper {
         );
       }
       console.log(this.projects);
+      this.connectDroppable();
+    }
+  
+    connectDroppable() {
+      const list = document.querySelector(`#${this.type}-projects ul`);
+  
+      list.addEventListener('dragenter', event => {
+        if (event.dataTransfer.types[0] === 'text/plain') {
+          list.parentElement.classList.add('droppable');
+          event.preventDefault();
+        }
+      });
+  
+      list.addEventListener('dragover', event => {
+        if (event.dataTransfer.types[0] === 'text/plain') {
+          event.preventDefault();
+        }
+      });
+  
+      list.addEventListener('dragleave', event => {
+        if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+          list.parentElement.classList.remove('droppable');
+        }
+      });
+  
+      list.addEventListener('drop', event => {
+        const prjId = event.dataTransfer.getData('text/plain');
+        if (this.projects.find(p => p.id === prjId)) {
+          return;
+        }
+        document
+          .getElementById(prjId)
+          .querySelector('button:last-of-type')
+          .click();
+        list.parentElement.classList.remove('droppable');
+        // event.preventDefault(); // not required
+      });
     }
   
     setSwitchHandlerFunction(switchHandlerFunction) {
@@ -171,18 +221,18 @@ class DOMHelper {
         activeProjectsList.addProject.bind(activeProjectsList)
       );
   
-      const timerId = setTimeout(this.startAnalytics, 3000);
+      // const timerId = setTimeout(this.startAnalytics, 3000);
   
-      document.getElementById('stop-analytics-btn').addEventListener('click', () => {
-        clearTimeout(timerId);
-      });
+      // document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+      //   clearTimeout(timerId);
+      // });
     }
   
     static startAnalytics() {
       const analyticsScript = document.createElement('script');
-      analyticsScript.src = 'assets/scripts/projects/analytics.js';
+      analyticsScript.src = 'assets/scripts/analytics.js';
       analyticsScript.defer = true;
-      //document.head.append(analyticsScript);
+      document.head.append(analyticsScript);
     }
   }
   
