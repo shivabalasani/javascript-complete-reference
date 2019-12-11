982c6f1c558ca8b482c8375d92ca4f282de1d340
cslplasma <54950381+cslplasma@users.noreply.github.com>
2019-12-11 17:11:15 -0500

    Modular JavaScript

---
+++ assets/scripts/projects/adv-dom-module.js
@@ -0,0 +1,25 @@
+import { ProjectList } from "./Module/ProjectList.js";
+
+globalThis.DEFAULT_VALUE = "MAX";
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList)
+    );
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
+    );
+  }
+
+  static startAnalytics() {
+    const analyticsScript = document.createElement("script");
+    analyticsScript.src = "assets/scripts/Utility/Analytics.js";
+    analyticsScript.defer = true;
+    document.head.append(analyticsScript);
+  }
+}
+
+App.init();
