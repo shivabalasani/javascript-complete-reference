1376f39bcf7f056d6eafdde84d1df21669a3bad0
shivabalasani <shiva.balasani@gmail.com>
2020-01-05 09:20:54 -0500

    function constructor

---
+++ assets/scripts/oop/function-constructor.js
@@ -0,0 +1,37 @@
+//Function constructor creates Objects
+function Person(firstname, lastname) {
+    console.log(this);
+    this.firstname = firstname;
+    this.lastname = lastname;
+    console.log('This function is invoked.');
+}
+
+Person.prototype.getFullName = function() {
+    return this.firstname + ' ' + this.lastname;   
+}
+
+var john = new Person('John', 'Doe');
+console.log(john);
+
+var jane = new Person('Jane', 'Doe');
+console.log(jane);
+
+Person.prototype.getFormalFullName = function() {
+    return this.lastname + ', ' + this.firstname;   
+}
+
+console.log(john.getFormalFullName());
+
+
+
+
+//Built in function constructors
+String.prototype.isLengthGreaterThan = function(limit) {
+    return this.length > limit;  
+}
+
+console.log("John".isLengthGreaterThan(3));
+
+Number.prototype.isPositive = function() {
+    return this > 0;   
+}
