e74ff83058e5884cf84c2bb7b0838b7d4eba5953
cslplasma <54950381+cslplasma@users.noreply.github.com>
2020-02-27 14:02:31 -0500

    Error Handling

---
+++ assets/scripts/exception-handling/async-error-handling.js
@@ -0,0 +1,31 @@
+//Error handling with Promise
+Promise.resolve('asyncfail')
+    .then(response => {
+        console.log(response)
+        throw new Error('#1 fail')
+    })
+    .then(response => {
+        console.log(response)
+    })
+    .catch(err => {
+        console.error('error', err.message)
+    })
+    .then(response => {
+        console.log('hi am I still needed?', response)
+        return 'done'
+    })
+    .catch(err => {
+        console.error(err)
+        return 'failed'
+    })
+
+//Error handling with async-await
+(async function() {
+    try {
+        await Promise.reject('oopsie')
+    } catch (err) {
+        console.error(err)
+    }
+
+    console.log('This is still good!')
+})()
